6a3253a3dea00a561f4c145e37453d0d312aac4b
rf69100 <fonseca.ryan69100@gmail.com>
2026-01-27 00:14:43 +0100

    version fonctionnelle finale

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,7 @@ import {
   parcoursBadgeStyles
 } from './content';
 
-// ============================================================================
-// CONSTANTES
-// ============================================================================
-
-const PROJECT_BASE_URL = process.env.REACT_APP_PROJECT_BASE_URL || '';
+const PROJECT_BASE_URL = 'https://ryanfonseca.fr/';
 
 // ============================================================================
 // COMPOSANTS UTILITAIRES
